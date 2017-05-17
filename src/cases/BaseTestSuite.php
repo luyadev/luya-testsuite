5d05298267bfba8f4ba68dddae4889426ab1701a
Basil Suter <git@nadar.io>
2017-05-17 11:59:43 +0200

    add phpdocs

--- src/cases/BaseTestSuite.php
+++ src/cases/BaseTestSuite.php
@@ -6,18 +6,58 @@ use luya\Boot;
 
 require_once 'vendor/autoload.php';
 
+/**
+ * Base Test Suite.
+ * 
+ * Usage:
+ * 
+ * ```php
+ * class MyTestCase extends BaseTestSuite
+ * {
+ *     public function getConfigArray()
+ *     {
+ *         return [
+ *            'id' => 'mytestapp',
+ *            'basePath' => dirname(__DIR__),
+ *         ];
+ *     }
+ * }
+ * ```
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
+ */
 abstract class BaseTestSuite extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var \luya\Boot
+     */
     public $boot;
+    
+    /**
+     * @var \luya\web\Application
+     */
     public $app;
 
+    /**
+     * Provide Configurtion Array.
+     */
     abstract public function getConfigArray();
     
+    /**
+     * Method which is executed beofre Setup in order to inject data on before Setup.
+     * 
+     * Make sure to call the parent before Setup.
+     */
     public function beforeSetup()
-    {
-        
+    {  
     }
 
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
     public function setUp()
     {
         $this->beforeSetup();
@@ -31,6 +71,11 @@ abstract class BaseTestSuite extends \PHPUnit\Framework\TestCase
         $this->app = $boot->app;
     }
 
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::tearDown()
+     */
     public function tearDown()
     {
         unset($this->app, $this->boot);
--- src/cases/WebApplicationTestCase.php
+++ src/cases/WebApplicationTestCase.php
@@ -2,10 +2,39 @@
 
 namespace luya\testsuite\cases;
 
+/**
+ * Web Application Test Case.
+ * 
+ * Provdides basic setup for Script paths in order make the urlManager work.
+ * 
+ * Usage:
+ * 
+ * ```php
+ * class MyTestCase extends WebApplicationTestCase
+ * {
+ *     public function getConfigArray()
+ *     {
+ *         return [
+ *            'id' => 'mytestapp',
+ *            'basePath' => dirname(__DIR__),
+ *         ];
+ *     }
+ * }
+ * ```
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
+ */
 abstract class WebApplicationTestCase extends BaseTestSuite
 {
+    /**
+     * {@inheritDoc}
+     * @see \luya\testsuite\cases\BaseTestSuite::beforeSetup()
+     */
     public function beforeSetup()
     {
+        parent::beforeSetup();
+        
         $_SERVER['SCRIPT_FILENAME'] = 'index.php';
         $_SERVER['SCRIPT_NAME'] =  '/index.php';
         $_SERVER['REQUEST_URI'] = '/';
