8663c1e840dc1853ed946402222117bd1bd98d22
Basil Suter <git@nadar.io>
2017-12-13 13:03:02 +0100

    improve cms block test case, closes #5

--- src/cases/CmsBlockTestCase.php
+++ src/cases/CmsBlockTestCase.php
@@ -4,8 +4,49 @@ namespace luya\testsuite\cases;
 
 use yii\base\InvalidConfigException;
 
+/**
+ * LUYA Cms Block TestCase.
+ * 
+ * Testing a CMS Block for the CMS Layout. Example usage:
+ * 
+ * ```php
+ * class TextBlockTest extends GenericBlockTestCase
+ * {
+ *     public $blockClass = 'luya\generic\blocks\TextBlock';
+ *     
+ *     public function getConfigArray()
+ *     {
+ *         return [
+ *             'id' => 'testTextBlock',
+ *             'basePath' => dirname(__DIR__),
+ *         ];
+ *     }
+ *     
+ *     public function testAdminAndFrontendRender()
+ *     {
+ *         $this->assertSame('<p>Removes spaces and br from frontend view.</p>', $this->renderFrontendNoSpace());
+ *         $this->assertSame('<p>Admin View</p>', $this->renderAdminNoSpace());
+ *     }
+ *     
+ *     public function testFrontendWithVars()
+ *     {
+ *         $this->block->setVarValues([
+ *              'text' => 'Hello World',
+ *         ]);
+ *         
+ *         $this->assertSame('<p>Hello World', $this->renderFrontendNoSpace());
+ *     }
+ * } 
+ * ```
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.6
+ */
 abstract class CmsBlockTestCase extends WebApplicationTestCase
 {
+    /**
+     * @var string The path to the block to be tested.
+     */
     public $blockClass;
     
     /**
@@ -13,6 +54,9 @@ abstract class CmsBlockTestCase extends WebApplicationTestCase
      */
     public $block;
     
+    /**
+     * @inheritdoc
+     */
     public function afterSetup()
     {
         parent::afterSetup();
@@ -25,6 +69,11 @@ abstract class CmsBlockTestCase extends WebApplicationTestCase
         $this->block = new $class();
     }
     
+    /**
+     * Renders the frontend view of a block.
+     * 
+     * @return string
+     */
     public function renderFrontend()
     {
         $this->assertNotEmpty($this->block->blockGroup());
@@ -37,6 +86,11 @@ abstract class CmsBlockTestCase extends WebApplicationTestCase
         return $this->block->renderFrontend();
     }
     
+    /**
+     * Renders the admin view of a blockl.
+     * 
+     * @return string
+     */
     public function renderAdmin()
     {
         $twig = new \Twig_Environment(new \Twig_Loader_Filesystem());
@@ -44,17 +98,23 @@ abstract class CmsBlockTestCase extends WebApplicationTestCase
         return $temp->render(['cfgs' => $this->block->getCfgValues(), 'vars' => $this->block->getVarValues()]);
     }
     
+    /**
+     * Render the admin view, but auto remove all spaces.
+     * 
+     * @return mixed
+     */
     public function renderAdminNoSpace()
     {
-        $text = trim(preg_replace('/\s+/', ' ', $this->renderAdmin()));
-        
-        return str_replace(['> ', ' <'], ['>', '<'], $text);
+        return $this->trimContent($this->renderAdmin());
     }
     
+    /**
+     * Render the frontend view, but auto remove all spaces.
+     * 
+     * @return string
+     */
     public function renderFrontendNoSpace()
     {
-        $text = trim(preg_replace('/\s+/', ' ', $this->renderFrontend()));
-        
-        return str_replace(['> ', ' <'], ['>', '<'], $text);
+        return $this->trimContent($this->renderFrontend());
     }
 }
