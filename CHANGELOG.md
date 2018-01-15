ff2d083e78fb535e1cd946ea2d80706a66a9b021
Basil Suter <git@nadar.io>
2018-01-15 13:46:09 +0100

    Added invokeMethod function to call protected and private methods.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ All notable changes to this project will be documented in this file. This projec
 
 1.0.8 (in progress)
 -------------------
++ Added invokeMethod function to call protected and private methods.
 
 1.0.7 (12. January 2018)
 -------------------
--- src/cases/BaseTestSuite.php
+++ src/cases/BaseTestSuite.php
@@ -141,4 +141,34 @@ abstract class BaseTestSuite extends \PHPUnit\Framework\TestCase
     {
         return $this->assertContains($this->trimContent($needle), $this->trimContent($haystack));
     }
+    
+    /**
+     * Call a private or protected method from an object and return the value.
+     * 
+     * ```php
+     * public function testProtectedMethod()
+     * {
+     *     // assuming MyObject as a protected method like:
+     *     // protected function hello($title)
+     *     // {
+     *     //     return $title;
+     *     // }
+     *     $object = new MyObject();
+     *     
+     *     $this->assertSame('Hello World', $this->invokeMethod($object, 'hello', ['Hello World']));
+     * }
+     * ```
+     * 
+     * @param object $object The object the method exists from.
+     * @param string $methodName  The name of the method which should be called.
+     * @param array $parameters An array of paremters which should be passed to the method.
+     * @return mixed
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
 }
